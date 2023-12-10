ea28f57a022c7bb367faa765b37870a6b04a90e6
glowingstone124 <hanserofficial@outlook.com>
2023-12-10 22:11:55 +0800

    added dynamic content switcher

--- js/index.js
+++ js/index.js
@@ -13,9 +13,47 @@ function determineTheme() {
         themeLink.href = 'styles/main.css';
     }
 }
-
-// 初始加载时调用一次
 determineTheme();
-
-// 在窗口大小变化时重新检测主题
 window.addEventListener('resize', determineTheme);
+document.addEventListener('DOMContentLoaded', function () {
+    const categoriesContainer = document.getElementById('categoriesContainer');
+    const categoryContentsContainer = document.getElementById('categoryContentsContainer');
+    let categoryElements = [];
+
+    const categories = ['分类1', '分类2'];
+    const contents = [
+        [
+            { title: '这是标题1', description: '这是简介1' },
+            { title: '这是标题2', description: '这是简介2' },
+            { title: '这是标题3', description: '这是简介3' }
+        ],
+        [
+            { title: '这是标题4', description: '这是简介4' },
+            { title: '这是标题5', description: '这是简介5' },
+            { title: '这是标题6', description: '这是简介6' }
+        ]
+    ];
+    categories.forEach(function (category, index) {
+        const categoryElement = document.createElement('h1');
+        categoryElement.classList.add('category');
+        categoryElement.textContent = category;
+        categoryElement.setAttribute('data-category', 'category' + (index + 1));
+        categoryElement.addEventListener('click', function () {
+            categoryContentsContainer.innerHTML = '';
+            contents[index].forEach(function (content) {
+                const contentElement = document.createElement('div');
+                contentElement.classList.add('rectangle');
+                contentElement.innerHTML = `<h1>${content.title}</h1><p>${content.description}</p>`;
+                categoryContentsContainer.appendChild(contentElement);
+            });
+            categoryElements.forEach(function (element) {
+                element.classList.remove('active');
+            });
+            categoryElement.classList.add('active');
+        });
+
+        categoriesContainer.appendChild(categoryElement);
+        categoryElements.push(categoryElement);
+    });
+    categories[0] && categoryElements[0].click();
+});
